a427b017277d6c644775cd896950186be5f64c1f
Cesar Silva <101893633+cesarsdcf@users.noreply.github.com>
2023-06-12 11:52:46 -0300

    Update index.tsx

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -12,7 +12,7 @@ export const ListItem = ({ item, onChange }: Props) => {
   return (
     <C.Container done={item.done}>
       <input type='checkbox' checked={item.done} onChange={e => onChange(item.id, e.target.checked)}/>
-      <label>{item.name} - {item.done.toString()}</label>
+      <label>{item.name}</label>
     </C.Container>
   );
 };
